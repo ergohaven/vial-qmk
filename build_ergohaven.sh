da98d01d9f3bf02ee27136b8557d766f422e955a
Danil Tolkachev <dandur.oak@gmail.com>
2024-03-12 18:58:02 +0300

    add macropad to build script

--- build_ergohaven.sh
+++ build_ergohaven.sh
@@ -6,6 +6,7 @@
 ./util/docker_cmd.sh make ergohaven/k03:enc-left-right
 ./util/docker_cmd.sh make ergohaven/k03:enc-right
 ./util/docker_cmd.sh make ergohaven/k03:ergohaven
+./util/docker_cmd.sh make ergohaven/macropad:ergohaven
 ./util/docker_cmd.sh make ergohaven/planeta/rev1:v1
 ./util/docker_cmd.sh make ergohaven/planeta/rev2:v2
 ./util/docker_cmd.sh make ergohaven/planeta/rev2:hid
