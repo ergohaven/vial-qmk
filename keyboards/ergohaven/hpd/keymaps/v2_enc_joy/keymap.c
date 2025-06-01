5aa2250b5172f088b6f4e0d1856748010abd787f
Danil Tolkachev <dandur.oak@gmail.com>
2025-06-02 00:15:25 +0300

    rename to enc_joy

--- keyboards/ergohaven/hpd/keymaps/v2_enc_joy/keymap.c
+++ keyboards/ergohaven/hpd/keymaps/v2_enc_joy/keymap.c
@@ -10,7 +10,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,   KC_A,   KC_S,    KC_D,     KC_F,     KC_G,                                              KC_H,  KC_J,     KC_K,     KC_L,   KC_SCLN, KC_QUOT,
         KC_LSFT,  KC_Z,   KC_X,    KC_C,     KC_V,     KC_B,                                              KC_N,  KC_M,     KC_COMM,  KC_DOT, KC_SLSH, KC_RSFT,
                           KC_PGDN, KC_PGUP,  KC_LCTL,  LOWER, KC_LALT, KC_SPACE,         KC_ENT, KC_RGUI, RAISE, KC_RCTL,  KC_LBRC,  KC_RBRC,
-                                                                       KC_MUTE,          KC_MUTE
+                                                                       KC_MUTE,          MS_BTN1
     ),
 
     [_LOWER] = LAYOUT(
